964551585b1ec153693c88e7648250f49bb9d345
Weylin <weylin@users.noreply.github.com>
2017-04-24 13:42:14 -0700

    Fixup Vagrant; works as intended now. (#32)

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,8 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
-  config.vm.provision :shell, path: "vagrant-bootstrap.sh"
+  config.vm.synced_folder ".", "/home/ubuntu/bot"
+  config.vm.provision :shell, path: "vagrant-bootstrap.sh", privileged: false
   config.vm.provider :virtualbox do |vb|
     vb.customize ["modifyvm", :id, "--memory", "2048", "--cpus", "2"]
   end
--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Update sources.
-sudo apt-get update
-
 # Install nodejs and npm from latest.
 # Do this from nodesource (Chris Lea PPA) as upstream Ubuntu packaging is behind.
 # https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
@@ -10,13 +7,17 @@ sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install gulp and pm2 globally.
-sudo npm install --global gulp pm2
+sudo npm install --global gulp-cli pm2
 
-# create link from project to ~/bot
-ln -sf /vagrant /home/ubuntu/bot
+# Source our home directory.
+home='/home/ubuntu'
 
 # Do the npm build stuff.
-cd bot && npm install && gulp
+mkdir $home/node_modules
+cd $home/bot
+ln -s $home/node_modules/ $home/bot/node_modules
+npm install
+gulp
 
 # Daemonize an instance. http://pm2.keymetrics.io/
-pm2 start /home/ubuntu/bot/bin/sweeper.js
+pm2 start $home/bot/bin/sweeper.js
