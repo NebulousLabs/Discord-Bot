d85b0a89e6c36a51654a5e5f64de008935ea727f
D0cR3d <Mike@Layer7.Solutions>
2017-07-14 13:52:42 -0500

    Added confirmation before delete/reset and help text updates (#74)
    * Update to notes, restricting access
    
    - Disable ability to run command in DM
    - Restrict access to command
     - Vanguard, Discord Chat Mods can do any command
     - Mod Assistant can do any command except delete
    - Renamed 'set' to 'add'
    - Basic error update to remind to be less than 255 characters
    
    * Update to Notes, changed DB Table & functionality.
    
    - Updated some of the error messages to be more informative.
    - Update Notes to use the `ModActions` table.
    - Removed 255 character note constraint limit.
    - Date now shows with the history command.
    - Notes must not be empty.
    - Added ability to delete all notes.
     - TODO: Add confirmation before delete.
    
    * Notes data types update
    
    - Updated Notes data types to TEXT to match DB table field.
    
    * Fix TSLint errors
    
    - Adjusted semicolons to fix lint errors
    
    * - Further updates to help text to fit one line.
    - Added missing semicolon.
    
    * - Add another missing semicolon.
    
    * Update Note.ts
    
    - Removed double periods on some messages.
    - Removed trying to username mention in header field.
    
    * Update Note.ts
    
    - Add missing $ for variable

--- src/commands/profile/Note.ts
+++ src/commands/profile/Note.ts
@@ -1,5 +1,5 @@
 import { Command } from 'yamdbf';
-import { GuildMember, Message, RichEmbed, Role, User } from 'discord.js';
+import { Collection, GuildMember, Message, RichEmbed, Role, User } from 'discord.js';
 import Database from '../../database/Database';
 
 const credentials = require('../../database.json');
@@ -14,10 +14,10 @@ export default class Note extends Command {
 			desc: 'Add a note for a user',
 			usage: '<prefix>note <Argument>',
 			info: 'Argument information below...\u000d\u000d' +
-			'add <User> <Note>  : Adds a note for specified user\u000d' +
-			'history <User>     : Displays note history for specified user\u000d' +
-			'delete <User> <ID> : Deletes specific note for specified user\u000d' +
-			'reset <User>       : Deletes all notes for specified user',
+			'add <User> <Note>  : Adds a note for user\u000d' +
+			'history <User>     : Displays note history for user\u000d' +
+			'delete <User> <ID> : Deletes specific note for user\u000d' +
+			'reset <User>       : Deletes all notes for user',
 			group: 'profile',
 			guildOnly: true,
 			roles: ['The Vanguard', 'Discord Chat Mods', 'Mod Assistant']
@@ -61,7 +61,7 @@ export default class Note extends Command {
 					this.database.commands.notes.get(message.guild.id, user.id)
 						.then(results => {
 							if (!results.length) {
-								message.channel.send(`There are no notes for ${user.username}.`);
+								message.channel.send(`There are no notes for <@${user.id}>.`);
 								return message.channel.stopTyping();
 							} else {
 								let notes: string = '';
@@ -76,7 +76,7 @@ export default class Note extends Command {
 						})
 						.catch(error => {
 							console.error(error);
-							message.channel.send(`There was an error while fetching the notes for ${user.username}.`);
+							message.channel.send(`There was an error while fetching the notes for <@${user.id}>.`);
 							return message.channel.stopTyping();
 						});
 					break;
@@ -84,6 +84,7 @@ export default class Note extends Command {
 					message.channel.send(`Sorry, but you do not have the role(s) necessary to access this command. Roles required: \`The Vanguard\` or \`Discord Chat Mods\` or \`Mod Assistant\`.`);
 					return message.channel.stopTyping();
 				}
+
 			case 'a':
 			case 'add':
 				if (author.roles.has(modRoles[0].id) || author.roles.has(modRoles[1].id) || author.roles.has(modRoles[2].id)) {
@@ -95,12 +96,12 @@ export default class Note extends Command {
 					} else {
 						this.database.commands.notes.create(message.guild.id, message.author.id, user.id, note)
 							.then(result => {
-								message.channel.send(`Successfully stored note for ${user.username}.`);
+								message.channel.send(`Successfully stored note for <@${user.id}>.`);
 								return message.channel.stopTyping();
 							})
 							.catch(error => {
 								console.error(error);
-								message.channel.send(`There was an error while storing the note for ${user.username}.`);
+								message.channel.send(`There was an error while storing the note for <@${user.id}>.`);
 								return message.channel.stopTyping();
 							});
 						break;
@@ -110,8 +111,16 @@ export default class Note extends Command {
 					return message.channel.stopTyping();
 				}
 
+			case 'd':
 			case 'delete':
 				if (author.roles.has(modRoles[0].id) || author.roles.has(modRoles[1].id)) {
+					// create confirmation filter
+					const setFilter: any = (m: Message) => {
+						if (m.author.id === message.author.id && m.content.match(/y|n/i))
+							return true;
+					};
+
+					// Check if user has any notes
 					if (isNaN(args[2])) {
 						message.channel.send(`Invalid note specified.`);
 						return message.channel.stopTyping();
@@ -120,7 +129,7 @@ export default class Note extends Command {
 					this.database.commands.notes.get(message.guild.id, user.id)
 						.then(results => {
 							if (!results.length) {
-								message.channel.send(`There are no notes for ${user.username}.`);
+								message.channel.send(`There are no notes for <@${user.id}>.`);
 								return message.channel.stopTyping();
 							} else {
 								let noteIndex: number = Number(args[2]);
@@ -129,42 +138,111 @@ export default class Note extends Command {
 									return message.channel.stopTyping();
 								}
 
-								this.database.commands.notes.delete(results[noteIndex - 1].id)
-									.then(result => {
-										message.channel.send(`Deleted note #${noteIndex} for ${user.username}.`);
-										return message.channel.stopTyping();
-									})
-									.catch(error => {
-										console.error(error);
-										message.channel.send(`There was an error while deleting a note for ${user.username}.`);
-										return message.channel.stopTyping();
-									});
+								// send confirmation message
+								message.channel.send(`Are you sure you want to delete the note for <@${user.id}> (__y__es | __n__o).`).then(() => {
+									// send awaitMessage
+									message.channel.awaitMessages(setFilter, {max: 1, time: 20000})
+										// user responded
+										.then((collected: Collection<string, Message>) => {
+											// yes, delete it
+											if (collected.first().content.charAt(0).toLowerCase() === 'y') {
+												this.database.commands.notes.delete(results[noteIndex - 1].id)
+													.then(result => {
+														message.channel.send(`Deleted note #${noteIndex} for <@${user.id}>.`);
+														return message.channel.stopTyping();
+													})
+													.catch(error => {
+														console.error(error);
+														message.channel.send(`There was an error while deleting the note for <@${user.id}>.`);
+														return message.channel.stopTyping();
+													});
+											}
+
+											// no, cancel the action
+											if (collected.first().content.charAt(0).toLowerCase() === 'n') {
+												message.channel.send('Cancelling action.');
+												return message.channel.stopTyping();
+											}
+										})
+
+										// user did not respond
+										.catch(() => {
+											// display output
+											message.channel.send('There was no collected message that passed the filter within the time limit!');
+											return message.channel.stopTyping();
+										});
+								});
 							}
 						})
 						.catch(error => {
 							console.error(error);
-							message.channel.send(`There was an error while processing a note deletion request for ${user.username}.`);
+							message.channel.send(`There was an error while processing a note deletion request for <@${user.id}>.`);
 							return message.channel.stopTyping();
 						});
-					break;
+						break;
 				} else {
 					message.channel.send(`Sorry, but you do not have the role(s) necessary to access this command. Roles required: \`The Vanguard\` or \`Discord Chat Mods\`.`);
 					return message.channel.stopTyping();
 				}
 
+			case 'r':
 			case 'reset':
 				if (author.roles.has(modRoles[0].id) || author.roles.has(modRoles[1].id)) {
-					this.database.commands.notes.reset(message.guild.id, user.id)
-						.then(result => {
-							message.channel.send(`Deleted all notes for ${user.username}.`);
-							return message.channel.stopTyping();
+					// create confirmation filter
+					const setFilter: any = (m: Message) => {
+						if (m.author.id === message.author.id && m.content.match(/y|n/i))
+							return true;
+					};
+
+					// Check if user has any notes
+					this.database.commands.notes.get(message.guild.id, user.id)
+						.then(results => {
+							if (!results.length) {
+								message.channel.send(`There are no notes for <@${user.id}>.`);
+								return message.channel.stopTyping();
+							} else {
+								// send confirmation message
+								message.channel.send(`Are you sure you want to delete all notes for <@${user.id}> (__y__es | __n__o).`).then(() => {
+									// send awaitMessage
+									message.channel.awaitMessages(setFilter, {max: 1, time: 20000})
+										// user responded
+										.then((collected: Collection<string, Message>) => {
+											// yes, delete it
+											if (collected.first().content.charAt(0).toLowerCase() === 'y') {
+												this.database.commands.notes.reset(message.guild.id, user.id)
+													.then(result => {
+														message.channel.send(`Deleted all notes for <@${user.id}>.`);
+														return message.channel.stopTyping();
+													})
+													.catch(error => {
+														console.error(error);
+														message.channel.send(`There was an error while deleting the notes for <@${user.id}>.`);
+														return message.channel.stopTyping();
+													});
+											}
+
+											// no, cancel the action
+											if (collected.first().content.charAt(0).toLowerCase() === 'n') {
+												message.channel.send('Cancelling action.');
+												return message.channel.stopTyping();
+											}
+										})
+
+										// user did not respond
+										.catch(() => {
+											// display output
+											message.channel.send('There was no collected message that passed the filter within the time limit!');
+											return message.channel.stopTyping();
+										});
+								});
+							}
 						})
 						.catch(error => {
 							console.error(error);
-							message.channel.send(`There was an error while deleting the notes for ${user.username}.`);
+							message.channel.send(`There was an error while processing a note deletion request for <@${user.id}>.`);
 							return message.channel.stopTyping();
 						});
-					break;
+						break;
 				} else {
 					message.channel.send(`Sorry, but you do not have the role(s) necessary to access this command. Roles required: \`The Vanguard\` or \`Discord Chat Mods\`.`);
 					return message.channel.stopTyping();
