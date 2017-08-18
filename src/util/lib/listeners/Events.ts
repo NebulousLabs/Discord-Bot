cc491e4415856cdda710394c2bff754e74a1c1db
Aleks Clark <aclark@ebackpack.com>
2017-08-18 11:19:13 -0500

    enable role reaction

--- src/util/lib/listeners/Events.ts
+++ src/util/lib/listeners/Events.ts
@@ -4,6 +4,7 @@ import { SweeperClient } from '../SweeperClient';
 import { MuteManager } from '../../../lib/mod/managers/MuteManager';
 import Constants from '../../Constants';
 
+
 const config: any = require('../../../config.json');
 const { on, registerListeners } = ListenerUtil;
 
@@ -32,113 +33,22 @@ export class Events {
 		const reactionAuthor: GuildMember = await reaction.message.guild.fetchMember(user);
 		let guildStorage: GuildStorage = await this._client.storage.guilds.get(reaction.message.guild.id);
 
-		let platformMessageId: string = await guildStorage.get('Role Reaction Message');
-		let spoilersMessageId: string = await guildStorage.get('Spoiler Reaction Message');
-		let factionMessageId: string = await guildStorage.get('Faction Reaction Message');
+		let roleMessageId: string = await guildStorage.get('Role Reaction Message');
 
-		let roles: Array<Role> = new Array();
+		let roles = this.fetchRoles(reaction);
 
-		// Platform
-		roles[0] = reaction.message.guild.roles.find('name', 'PC');
-		roles[1] = reaction.message.guild.roles.find('name', 'Playstation');
-		roles[2] = reaction.message.guild.roles.find('name', 'Xbox');
-		// Spoilers Enabled
-		roles[3] = reaction.message.guild.roles.find('name', 'Spoilers Enabled');
-		// Factions
-		roles[4] = reaction.message.guild.roles.find('name', 'Dead Orbit');
-		roles[5] = reaction.message.guild.roles.find('name', 'Future War Cult');
-		roles[6] = reaction.message.guild.roles.find('name', 'New Monarchy');
-
-		// Platform Message
-		if (reaction.message.id === platformMessageId) {
-			switch (reaction.emoji.name) {
-				case 'blizz':
-					if (reactionAuthor.roles.has(roles[0].id)) return reaction.remove(user);
-					else return await reactionAuthor.addRole(roles[0]);
-
-				case 'ps':
-					if (reactionAuthor.roles.has(roles[1].id)) return reaction.remove(user);
-					else return await reactionAuthor.addRole(roles[1]);
-
-				case 'xb':
-					if (reactionAuthor.roles.has(roles[2].id)) return reaction.remove(user);
-					else return await reactionAuthor.addRole(roles[2]);
+		// Role Reaction Message
+		if (reaction.message.id === roleMessageId) {
+			let role_name = reaction.emoji.name;
+			let role = roles[role_name];
+			if (role) {
+				if (reactionAuthor.roles.has(role.id)) return reaction.remove(user);
+				else return await reactionAuthor.addRole(role);
 			}
+			
 		}
 
-		// Spoiler Message
-		if (reaction.message.id === spoilersMessageId) {
-			switch (reaction.emoji.name) {
-				case 'D2':
-					if (reactionAuthor.roles.has(roles[3].id)) return reaction.remove(user);
-					else return await reactionAuthor.addRole(roles[3]);
-			}
-		}
-
-		// Faction Wars Message
-		if (reaction.message.id === factionMessageId) {
-			switch (reaction.emoji.name) {
-				case 'do':
-					// If they already have the role, remove it so they can re-react to add it.
-					if (reactionAuthor.roles.has(roles[4].id))
-						reaction.remove(user);
-					// Else if they don't have the role, give it to them.
-					else
-						await reactionAuthor.addRole(roles[4]);
-
-					// If they have FWC or NM then remove those roles from them
-					await reactionAuthor.removeRoles([roles[5], roles[6]]);
-					// and also remove their reactions
-					reaction.message.reactions.forEach(async (re: MessageReaction) => {
-						await re.fetchUsers();
-						if (re.emoji.name === 'do')
-							return;
-						await re.remove(user);
-					});
-
-					return;
-
-				case 'fwc':
-					// If they already have the role, remove it so they can re-react to add it.
-					if (reactionAuthor.roles.has(roles[5].id))
-						reaction.remove(user);
-					// Else if they don't have the role, give it to them.
-					else
-						await reactionAuthor.addRole(roles[5]);
-
-					// If they have DO or NM then remove those roles from them
-					await reactionAuthor.removeRoles([roles[4], roles[6]]);
-					// and also remove their reactions
-					reaction.message.reactions.forEach(async (re: MessageReaction) => {
-						await re.fetchUsers();
-						if (re.emoji.name === 'fwc')
-							return;
-						await re.remove(user);
-					});
-
-					return;
-
-				case 'nm':
-					// If they already have the role, remove it so they can re-react to add it.
-					if (reactionAuthor.roles.has(roles[6].id))
-						reaction.remove(user);
-					// Else if they don't have the role, give it to them.
-					else
-						await reactionAuthor.addRole(roles[6]);
-
-					// If they have DO or FWC then remove those roles from them
-					await reactionAuthor.removeRoles([roles[4], roles[5]]);
-					// and also remove their reactions
-					reaction.message.reactions.forEach(async (re: MessageReaction) => {
-						await re.fetchUsers();
-						if (re.emoji.name === 'nm')
-							return;
-						await re.remove(user);
-					});
-
-					return;
-			}
-		}
+		
 	}
 
 	@on('messageReactionRemove')
@@ -146,58 +56,20 @@ export class Events {
 		const reactionAuthor: GuildMember = await reaction.message.guild.fetchMember(user);
 		let guildStorage: GuildStorage = await this._client.storage.guilds.get(reaction.message.guild.id);
 
-		let platformMessageId: string = await guildStorage.get('Role Reaction Message');
-		let spoilersMessageId: string = await guildStorage.get('Spoiler Reaction Message');
-		let factionMessageId: string = await guildStorage.get('Faction Reaction Message');
+		let roleMessageId: string = await guildStorage.get('Role Reaction Message');
 
-		let roles: Array<Role> = new Array();
-
-		// Platform
-		roles[0] = reaction.message.guild.roles.find('name', 'PC');
-		roles[1] = reaction.message.guild.roles.find('name', 'Playstation');
-		roles[2] = reaction.message.guild.roles.find('name', 'Xbox');
-		// Spoilers Enabled
-		roles[3] = reaction.message.guild.roles.find('name', 'Spoilers Enabled');
-		// Factions
-		roles[4] = reaction.message.guild.roles.find('name', 'Dead Orbit');
-		roles[5] = reaction.message.guild.roles.find('name', 'Future War Cult');
-		roles[6] = reaction.message.guild.roles.find('name', 'New Monarchy');
+		let roles = this.fetchRoles(reaction);
 
 		// Platform Message
-		if (reaction.message.id === platformMessageId) {
-			switch (reaction.emoji.name) {
-				case 'blizz':
-					return await reactionAuthor.removeRole(roles[0]);
-
-				case 'ps':
-					return await reactionAuthor.removeRole(roles[1]);
-
-				case 'xb':
-					return await reactionAuthor.removeRole(roles[2]);
+		if (reaction.message.id === roleMessageId) {
+			let role_name = reaction.emoji.name;
+			let role = roles[role_name];
+			if (role) {
+				return await reactionAuthor.removeRole(role);
 			}
 		}
 
-		// Spoiler Message
-		if (reaction.message.id === spoilersMessageId) {
-			switch (reaction.emoji.name) {
-				case 'D2':
-					return await reactionAuthor.removeRole(roles[3]);
-			}
-		}
 
-		// Faction Wars Message
-		if (reaction.message.id === factionMessageId) {
-			switch (reaction.emoji.name) {
-				case 'do':
-					return await reactionAuthor.removeRole(roles[4]);
-
-				case 'fwc':
-					return await reactionAuthor.removeRole(roles[5]);
-
-				case 'nm':
-					return await reactionAuthor.removeRole(roles[6]);
-			}
-		}
 	}
 
 	@on('guildMemberRemove')
@@ -239,5 +111,13 @@ export class Events {
 	private async onMessage(message: Message): Promise<void>
 	{
 		// Saved for future use
+	}
+
+	private fetchRoles(reaction: MessageReaction): { [key:string]:Role} {
+		let roles: { [key:string]:Role} = {};
+		for (let role of Constants.SiaRoles) {
+			roles[role.name] = reaction.message.guild.roles.find('name', role.name);
+		}
+		return roles;
 	}
 }
